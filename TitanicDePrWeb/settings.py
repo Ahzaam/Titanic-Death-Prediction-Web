56374f67ec8b1b6b75da69a0506875ba1a3fcaa5
Ahzaam <99543407+Ahzaam@users.noreply.github.com>
2022-06-02 13:57:51 +0530

    heroku upload

--- TitanicDePrWeb/settings.py
+++ TitanicDePrWeb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)#(nm-rpg1e71m2e^m*9@9#=ofhyw+d10l-jl^16(ik2c*q(92
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
